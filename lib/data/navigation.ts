e1a1e6228359e54a783ff336817f2c64eb7b4f72
David AÏDASSO <davidaidasso252@gmail.com>
2026-08-14 15:33:15 +0100

    data nav

---
+++ lib/data/navigation.ts
@@ -0,0 +1,19 @@
+export const navigation = [
+  {
+    label: "Program",
+    href: "#program",
+  },
+  {
+    label: "Product",
+    href: "#product",
+    badge: "New",
+  },
+  {
+    label: "Events",
+    href: "#events",
+  },
+  {
+    label: "About",
+    href: "#about",
+  },
+] as const;
