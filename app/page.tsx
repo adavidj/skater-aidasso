5579ebac2402200e4849347ab90eee5789d34515
David AÏDASSO <davidaidasso252@gmail.com>
2026-08-14 16:16:26 +0100

    add benefits

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/sections/hero";
+import { Benefits } from "@/components/sections/benefits";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Benefits />
     </main>
   );
 }
